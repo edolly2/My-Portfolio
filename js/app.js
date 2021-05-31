fb29f0aebd93c6ecfaccb3ab7978c4fd33688bcc
edolly2 <edolly2@protonmail.com>
2021-05-30 23:18:13 -0500

    ""

--- js/app.js
+++ js/app.js
@@ -14,6 +14,8 @@ const gobackBtn = $('.goback-btn-cont');
 const gobackBtn2 = $('.goback-btn-cont2');
 const bioBtn = $('.bio-btn');
 const bio = $('.bio-cont');
+const popupExit = $('.popup-exit-btn');
+const popup = $('.popup-cont');
 
 
 
@@ -32,12 +34,22 @@ $(document).ready(() => {
         $(navMenuEnter).fadeIn(500);
     });
 
+    // $(navLink).on('mouseover', (e) => {
+    //     $(e.target).css({'background-color' : '#1ceaea', 'color' : 'yellow', 'box-shadow' : 'inset 0 0 2rem black'});
+    // });
+
+    // $(navLink).on('mouseleave', (e) => {
+    //     $(e.target).css({'background-color' : 'rgba(255, 255, 255, 0.05)', 'box-shadow' : '5px 5px 30px rgba(0, 0, 0, 0.2)', 'color' : '#1ceaea'});
+    // });
+
+   
     $(navLink).on('click', (e) => {
-        $(navLink).css({'background-color' : 'rgba(255, 255, 255, 0.05)', 'box-shadow' : '5px 5px 30px rgba(0, 0, 0, 0.2)', 'color' : '#1ceaea'});
-        $(e.target).css({'background-color' : '#1ceaea', 'color' : 'yellow', 'box-shadow' : 'inset 0 0 2rem black'});
+        $(e.currentTarget).add('.selected');
+        $(e.currentTarget).css('text-decoration', 'line-through');
     });
 
-    $('.nav-link').on('click', (e) => {
+    
+    $(navLink).on('click', (e) => {
         $('.modal').hide(500);
         if($(e.target).is('#about')) {
             $(aboutModal).show(500);
@@ -56,9 +68,9 @@ $(document).ready(() => {
         }
     });
 
-    $('.exit-btn').on('click', () => {
+    $('.exit-btn-modal').on('click', () => {
         $('.modal').hide(500);
-        $(navMenuEnter).show();
+        $(navMenu).show(500);
     });
 
     $(certsBtn).on('click', () => {
@@ -78,5 +90,63 @@ $(document).ready(() => {
         $(bio).hide(500);
         $(aboutModal).show(500);
     });
+
+    $(popupExit).on('click', () => {
+        $(popup).hide(500);
+    });
+
+    if($(popup).css('display') == 'block') {
+        $(navMenu).hide();
+    }
+
+    $('.email-me-exit-btn').on('click', () => {
+        $('.email-me-link-cont').hide(500);
+    });
+
+    $('.soci1').on('mouseover', () => {
+        $('.soci-title').css('color', 'yellow');
+    });
+
+    $('.soci1').on('mouseleave', () => {
+        $('.soci-title').css('color', 'white');
+    });
+
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    
+    $('.email-me-cont').on('click', () => {
+        var email = 'demo@demo.com';
+        var subject = 'Test';
+        var emailBody = 'Hi Sample,';
+        var attach = 'path';
+        if(isMobile) {
+            parent.location = "mailto:"+email+"?subject="+subject+"&body="+emailBody+
+                "?attach="+attach;
+        }
+        else {
+            $('.email-me-link-cont').show(500);
+        }
+    });
+    var currentMousePos = { x: -1, y: -1 };
+    $(document).mousemove(function(event) {
+        currentMousePos.x = event.pageX;
+        currentMousePos.y = event.pageY;
+        console.log(currentMousePos.x, currentMousePos.y);
+    });
 });
 
+function onMouseOut(event) {
+    if (event.clientY < 50) {
+    document.removeEventListener("mouseout", onMouseOut);
+    document.getElementById("popup").style.display = "block";
+    document.getElementById("navCont").style.display = "none";
+    }
+}
+document.addEventListener("mouseout", onMouseOut);
+
+//   var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+// 		var element = document.getElementById('text');
+// 		if (isMobile) {
+//   			element.innerHTML = "You are using Mobile";
+// 		} else {
+// 			element.innerHTML = "You are using Desktop";
+// 		}
