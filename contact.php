55d430a4acd7bdbba1cc803f63c584c86fcf5169
edolly2 <edolly2@protonmail.com>
2021-06-14 01:47:34 -0500

    ""

--- contact.php
+++ contact.php
@@ -1,46 +1,72 @@
 <?php 
 
-    $name = "";
-    $email = "";
-    $comment = "";
-    $nameErr = "";
-    $emailErr = "";
-    $commentErr = "";
+$name = "";
+$email = "";
+$comment = "";
+$nameErr = "";
+$emailErr = "";
+$commentErr = "";
+$ERRORS = false;
 // REQUIRED FIELDS AND VALIDATE
-    if($_SERVER["REQUEST_METHOD"] == "POST") {
-        if(empty($_POST["name"])) {
-            $nameErr = "*Name Required";
-        }
-        else {
-            $name = test_input($_POST["name"]);
-            if(!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
-                $nameErr = "*Only Letters and Spaces Allowed";
-            }
-        }
-        if(empty($_POST["email"])) {
-            $emailErr = "*Email Required";
-        }
-        else {
-            $email = test_input($_POST["email"]);
-            if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                $emailErr = "*Invalid Email Format";
-            }
-        }
-        if(empty($_POST["comment"])) {
-            $commentErr = "*Please Leave a Short Message With Any Concerns or Questions";
-        }
-        else{
-            $comment = test_input($_POST["comment"]);
+if($_SERVER["REQUEST_METHOD"] == "POST") {
+    if(empty($_POST["name"])) {
+        $nameErr = "*Name Required";
+        $ERRORS = true;
+    }
+    else {
+        $name = test_input($_POST["name"]);
+        if(!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
+            $nameErr = "*Only Letters and Spaces Allowed";
+            $ERRORS = true;
         }
     }
+    if(empty($_POST["email"])) {
+        $emailErr = "*Email Required";
+        $ERRORS = true;
+    }
+    else {
+        $email = test_input($_POST["email"]);
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailErr = "*Invalid Email Format";
+            $ERRORS = true;
+        }
+    }
+    if(! $_POST["comment"]) {
+        $commentErr = "*Please Leave a Short Message With Any Concerns or Questions";
+        $ERRORS = true;
+    }
+    else{
+        $comment = test_input($_POST["comment"]);
+    }
+}
 
-    // SANITIZE INPUT
-    function test_input($data) {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
-    }
+// $sanitized = false;
+
+// SANITIZE INPUT
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+// $entry = "Name: $name \n\n Email: $email\n\n Message: $comment";
+// file_put_contents("message.txt", $entry, FILE_APPEND);
+// echo "<h3>THANKS</h3>"
+    
+    
+    // foreach($_POST as $names => $value) {
+    //     if($ERRORS == false) {
+    //         $senderName = $_POST["name"];
+    //         $senderEmail = $_POST["email"];
+    //         $senderComment = $_POST["comment"];
+    //     }
+    //     $to = "Eric Dollinger<edolly2@gmail.com>";
+    //     $subject = "Portfolio Website Message";
+    //     $message = "MESSAGE FROM: " . $senderName . "\n\n EMAIL IS:" . $senderEmail . "\n\n MESSAGE:" . $sendercomment;
+    //     if(mail($to, $subject, $message)) {
+    //         echo "<h3>Your Message Was Sent.</h3>";
+    //     }
+    // }
 
 ?>
 
@@ -82,6 +108,7 @@
 <body class="form-page">
 
  <!-- WRITE ME -->
+ <!-- action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" -->
 
                     <div">
                         <div class="form-exit-btn-cont exit-cont">
@@ -89,7 +116,7 @@
                         </div>
                         <div class="form-cont">
                             <h2 class="form-title">Your Input Is Greatly Appreciated!</h2>
-                            <form class="write-me-form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>"> 
+                            <form class="write-me-form" method="post"> 
                                 <p><span class="red">* Required Field</span></p>
                                 <label for="name">Name <span class="red">*</span></label><br/>
                                 <input type="text" name="name" id="name" class="is-focus form-input" value="<?php echo $name; ?>">
@@ -97,9 +124,9 @@
                                 <label for="email">E-mail <span class="red">*</span></label><br/>
                                 <input type="text" name="email" id="email" class="is-focus form-input" value="<?php echo $email; ?>">
                                 <span class="error"><?php echo $emailErr; ?></span><br/>
-                                <label for="comments">Comments <span class="red">*</span></label><br/>
+                                <label for="comment">Comments <span class="red">*</span></label><br/>
                                 <span class="error"><?php echo $commentErr; ?></span><br/>
-                                <textarea name="comments" id="comments" class="is-focus" cols="38"
+                                <textarea name="comment" id="comment" class="is-focus" cols="38"
                                 rows="15"><?php echo $comment; ?></textarea><br />
                                 <input type="submit" value="Send" id="submit">
                             </form>
